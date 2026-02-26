46a40cdace314cf51ae2d7b16e8cddde95c121c2
David-Tarlos <david.tarlos@edu.tbz.ch>
2026-02-26 15:52:59 +0100

    fix: use 10.0.2.2 for Android emulator API connection

--- constants/api.ts
+++ constants/api.ts
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native';
 
 const BASE_URL = Platform.select({
-  android: 'http://localhost:3030',
+  android: 'http://10.0.2.2:3030',
   ios: 'http://localhost:3030',
   default: 'http://localhost:3030',
 }) as string;
